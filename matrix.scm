841b8e90c546f236971597189349a5f7f074867c
generoberts <48050400+generoberts@users.noreply.github.com>
2020-01-10 07:21:29 +0700

    checking matrix's dimention

--- matrix.scm
+++ matrix.scm
@@ -1,4 +1,4 @@
-;; all about matrix
+;;; all about matrix
 
 (define matrix
   (matrix lst size row col) ; matrix is just a list of lists
@@ -12,8 +12,17 @@
   (if (and (not (null? lst))
            (list? lst))
       (matrix lst
-              (cons (lenght (lst))
-                    (lenght (car lst)))
+              (cons (lenght lst)
+                    (matrix-col-num lst))
               (lenght lst)
-              (length (car lst)))
-      (raise "Not a matrix")))
+              (matrix-col-num lst)
+      (raise "Not a matrix"))))
+
+;; helper function
+;; if it's a vector, this function retrun 1
+;; else, it's a matrix (list of lists) so this function
+;; return the number of col
+(define (matrix-col-num lst)
+  (if (list? (car lst))
+      (length (car lst))
+      1))
