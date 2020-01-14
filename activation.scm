cfb869e935137366e73ee631d2fa054d7d7b012c
generoberts <48050400+generoberts@users.noreply.github.com>
2020-01-14 07:04:10 +0700

    logistic and softmax activation function

--- activation.scm
+++ activation.scm
@@ -1,4 +1,10 @@
-(define activation-functions '(sigmoid logistic softmax))
+;; https://en.wikipedia.org/wiki/Activation_function
+(define activation-functions '(sigmoid logistic softmax identity))
 
-(define (sigmoid val)
-  ())
+(define (logistic x)
+  (/ 1 (+ 1 (exp (- x)))))
+
+;; take a vector (or a list) as input
+(define (softmax vect)
+  (let ((summation (apply + (map exp vect))))
+    (map (lambda (val) (/ (exp val) summation)) vect)))
