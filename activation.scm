71e1a5a1af736d94c1a3dc817a518e2fa9f8a6e3
generoberts <48050400+generoberts@users.noreply.github.com>
2020-01-17 06:46:19 +0700

    add derivatievs of most activation functions

--- activation.scm
+++ activation.scm
@@ -9,43 +9,76 @@
 ;; a function that we don't implement here
 (define defined-functions '(sigmoid logistic softmax identity))
 
+(define-record-type activation-function
+  (activation-function type function derivative)
+  activation-function?
+  (type type) ; once define, activation function can't change its type
+  (function function)
+  (derivative derivative)) ; all derivative is named as function type but with prefix d
+
 ;; take a vector (or a list) as input
 (define (softmax vect)
   (let ((summation (apply + (map exp vect))))
     (map (lambda (val) (/ (exp val) summation)) vect)))
 
+;; todo
+;; (define (dsoftmax vect))
+
 ;; also take a vector (or a list) as input
 (define (maxout vect)
   (apply max vect))
+;; todo
+;; (defnie (dmaxout vect))
 
 ;; all other functions take a number as argument
 
 ;; identity function is defined in (scheme base)
+(define (didentity x)
+  1)
 
 (define (binary-step x)
   (if (>= x 0)
       1
       0))
+(define (dbinary-step x)
+  (if (= x 0)
+      1 ; here we hardcode it, since wikipedia doesn't specify the value
+      0))
 
 (define (logistic x)
   (/ 1 (+ 1 (exp (- x)))))
+(define (dlogistic x)
+  (* (logistic x) (- 1 (logistic x))))
 
 (define (tanh x)
   (define (e^x+-e^-x arg plus?)
     ((if plus? + -) (exp arg) (exp (- x))))
   (/ (e^x+-e^-x x #f)
      (e^x+-e^-x x #t)))
+(define (dtanh x)
+  (- 1 (expt (tanh x) 2)))
+
 
 (define (relu x)
   (if (> x 0)
       x
+      0))
+(define (drelu x)
+  (if (> x 0)
+      1
       0))
 
 (define (leaky-relu x)
   (if (>= x 0)
       x
       (* 0.01 x)))
+(define (dleaky-relu x)
+  (if (>= x 0)
+      1
+      0.01))
 
 (define (softplus x)
   (log (+ 1 (exp x))))
+(define (dsoftplus x)
+  (/ 1 (+ 1 (exp (- x)))))
 
