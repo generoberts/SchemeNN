b475e5bfa49a81c66caae99e3457b69deb50d74d
generoberts <48050400+generoberts@users.noreply.github.com>
2020-01-18 16:17:01 +0700

    cost function now can take both number and a list of number as arguments

--- cost.scm
+++ cost.scm
@@ -9,7 +9,13 @@
 ;; result is what feedforward gives us
 ;; expeted is what the training data is
 (define (mse result expected)
-  (* 0.5 (apply + (map (lambda (x) (* x x))
-                       (map - result expeted)))))
+  (if (and (number? result)
+           (number? expected)) ; the result is a number, that is we want to predict only 1 value
+      (* 0.5 (expt (- result expected) 2))
+      (* 0.5 (apply + (map (lambda (x) (* x x))
+                           (map - result expected))))))
 (define (gmse results expected)
-  (map - result expeted))
+  (if (and (number? result)
+           (number? expected))
+      (- result expected)
+      (map - result expeted)))
