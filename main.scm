5198adf0899c1e713f3f468ee50ed68920bd259b
generoberts <48050400+generoberts@users.noreply.github.com>
2020-01-18 18:37:34 +0700

    edit main to easier testting

--- main.scm
+++ main.scm
@@ -1,7 +1,7 @@
 (include "nn")
 
+(define l1 (make-layer 2 'tanh 1))
+(define m1 (model '(0.5 0.3) (list l1)))
 (define (main)
-  (define l1 (make-layer 2 'tanh 1))
-  (define m1 (model '(0.5 0.3) (list l1)))
   (print (feedforward m1))
   (print 'done))
